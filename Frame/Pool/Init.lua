47134c23e23d6bfce1ba5f0cdb85faa514620b4a
xiaohao <1289766329@qq.com>
2025-12-07 00:25:31 +0800

    1

---
+++ Frame/Pool/Init.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+local modules = {
+    "Frame.Pool.Pool",
+}
+
+function M.load()
+    for i = 1, #modules do
+        require(modules[i])
+    end
+end
+
+function M.unload()
+    for i = 1, #modules do
+        package.loaded[modules[i]] = nil
+    end
+end
+
+return M
